8e03a33fa12f6723ccad1b130cdf5aa7f5bf3e16
MadhurSikarwar <madhursikarwar1234@gmail.com>
2024-09-23 15:12:39 +0530

    Updateing the file first.cpp

--- first.cpp
+++ first.cpp
@@ -8,7 +8,7 @@ int main()
     int a = 1;
     int b = 2;
     cout<<"Adding a and b results in "<<(a+b);
-    //adding commnets
+    //adding commnets to the cpp file
     //adding more comments
     return 0;
 }
