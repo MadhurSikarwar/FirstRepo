099e554526fe9f1def3fa71fbccaf30f10a637ce
MadhurSikarwar <madhursikarwar1234@gmail.com>
2024-09-23 15:11:38 +0530

    Update first.cpp

--- first.cpp
+++ first.cpp
@@ -4,7 +4,10 @@ int main()
 {
     cout<<"This is a git hub repository";
     cout<<"Making changes in the git hub editor";
-    cout<<Making changes again";
+    cout<<"Making changes again";
+    int a = 1;
+    int b = 2;
+    cout<<"Adding a and b results in "<<(a+b);
     //adding commnets
     //adding more comments
     return 0;
